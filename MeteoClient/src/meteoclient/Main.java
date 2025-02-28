fffc20db2c635425b81a82cc9863cd0b251dac04
z3ater <147728174+z3ater@users.noreply.github.com>
2025-02-28 23:21:07 +0100

    Refactor MeteoClient functionality into Main class and remove obsolete MeteoClient.java

--- MeteoClient/src/meteoclient/Main.java
+++ MeteoClient/src/meteoclient/Main.java
@@ -5,6 +5,8 @@
 
 package meteoclient;
 
+import com.meteo.ServerMeteo1Service;
+import com.meteo.ServerMeteo1;
 /**
  *
  * @author amar
@@ -15,7 +17,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+   
+            ServerMeteo1Service service = new ServerMeteo1Service();
+            ServerMeteo1 port = service.getServerMeteo1Port();
+
+          
+            String ville = "Alger";
+            String pays = "Algeria";
+            String response = port.getWeather(ville, pays);
+
+   
+            System.out.println("Météo pour " + ville + ", " + pays + ": " + response);
+        } catch (Exception e) {
+            System.err.println("Erreur lors de l'appel du service météo: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 }
